30127fc7b41b55cced1a2feec30c468c077a9bbb
Kiwoong Kim <gw150311@naver.com>
2024-02-27 01:34:51 +0900

    this is third commit

--- gittest.py
+++ gittest.py
@@ -2,3 +2,5 @@
 def gittest():
    print("this is git test")
    print("already complete first commit.")
+   print("already complete first push and second commit.")
+   print("I will try second push")
